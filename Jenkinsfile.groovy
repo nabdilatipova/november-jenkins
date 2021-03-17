af18a23a1780fd70632e7d5803ec8d9ad69f67b3
farrukh90 <farrukhsadykov@gmail.com>
2021-03-17 23:38:11 +0000

    added 5 stages

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+# Scripted pipeline 
+node {
+    stage("Stage1"){
+        echo "Hello"
+    }
+    stage("Stage2"){
+        echo "Hello"
+    }
+    stage("Stage3"){
+        echo "Hello"
+    }
+    stage("Stage4"){
+        echo "Hello"
+    }
+    stage("Stage5"){
+        echo "Hello"
+    }
+}
+
+
+
